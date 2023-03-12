1ffe40b7711919389ff4bfd0b3df134022856237
Kingdox ~ Xavier Arpa ~ <arpaxavier@gmail.com>
2023-03-12 17:32:16 +0100

    - Avances.

--- assets/components/pagemode/pagemode.js
+++ assets/components/pagemode/pagemode.js
@@ -8,7 +8,11 @@ document.addEventListener(environment.dom.keys.onLoaded, function(event)
 {
     console.log("NEW WORLD");
     refresh_pallette(); //Cargamos la paleta actual
-    document.getElementById(ID_BUTTON_SWITCH).onclick = () =>  set_pagemode(get_pagemode()+1); // Avanzamos la paleta si presiona el botón
+    document.getElementById(ID_BUTTON_SWITCH).onclick = () =>  
+    {
+        console.log("SWITCH");
+        set_pagemode(get_pagemode()+1); // Avanzamos la paleta si presiona el botón
+    }
 });
 
 const get_pagemode = () => Number(environment.localStorage.get(environment.localStorage.keys.pagemode,"0"));

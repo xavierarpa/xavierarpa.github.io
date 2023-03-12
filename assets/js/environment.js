877258796500892dc3f2622b6baf39c3a3bee79e
Kingdox ~ Xavier Arpa ~ <arpaxavier@gmail.com>
2023-03-12 16:22:14 +0100

    - Avances.

---
+++ assets/js/environment.js
@@ -0,0 +1,14 @@
+export const environment = 
+{
+    
+    contact: 
+    {
+        email: "mailto:arpaxavier@gmail.com",
+        twitter: "https://twitter.com/_kingdox_",
+        github: "https://github.com/kingdox",
+        itchio: "https://kingdox.itch.io/",
+        develteam: "https://www.develteam.com/Developer/Kingdox",
+    },
+    
+
+};

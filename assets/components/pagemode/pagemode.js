33675d2c2aaad121a7ae0f358ee428b4c36c6534
Kingdox ~ Xavier Arpa ~ <arpaxavier@gmail.com>
2023-03-12 15:02:13 +0100

    - Avances.

--- assets/components/pagemode/pagemode.js
+++ assets/components/pagemode/pagemode.js
@@ -1,35 +1,20 @@
 const K_PAGE_MODE = 'pagemode';
-////////////////////////////////
-////////////////////////////////
 const K_PAGE_MODE_TYPE_DARK = 'Dark';
 const K_PAGE_MODE_TYPE_LIGHT = 'Light';
 const K_PAGE_MODE_TYPE_DEFAULT = K_PAGE_MODE_TYPE_DARK;
-////////////////////////////////
-////////////////////////////////
-if (localStorage.getItem(K_PAGE_MODE) == null)  localStorage.getItem(K_PAGE_MODE, K_PAGE_MODE_TYPE_DEFAULT);
-////////////////////////////////
-////////////////////////////////
+
+// by default assign page mode as default if chache info was not found !
+if (localStorage.getItem(K_PAGE_MODE) == null)  localStorage.setItem(K_PAGE_MODE, K_PAGE_MODE_TYPE_DEFAULT);
+
+// on DOM is loaded
 document.addEventListener("DOMContentLoaded", function(event) 
 {
-    console.log("pagemode.js");
     var switchButton = document.getElementById("switch_pageMode");
-////////////////////////////////
-////////////////////////////////
-    switchButton.onclick = function() 
-    {
-        setPageMode(localStorage.getItem(K_PAGE_MODE) == K_PAGE_MODE_TYPE_DARK);        
-    };
+    switchButton.onclick = () => setPageMode(localStorage.getItem(K_PAGE_MODE) == K_PAGE_MODE_TYPE_DARK);
 });
-////////////////////////////////
-////////////////////////////////
-function setPageMode(status)
+
+// func to switch "page mode"
+function setPageMode(status) 
 {
-    if(status)
-    {
-        localStorage.setItem(K_PAGE_MODE, 'Light');
-    }
-    else
-    {
-        localStorage.setItem(K_PAGE_MODE, 'Dark');
-    }
+    localStorage.setItem(K_PAGE_MODE, (status ? K_PAGE_MODE_TYPE_LIGHT : K_PAGE_MODE_TYPE_DARK));
 }

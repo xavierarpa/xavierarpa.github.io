8cfc83c98d24467b51c1efdc3aa7de7607e3e7e9
Kingdox ~ Xavier Arpa ~ <arpaxavier@gmail.com>
2023-03-12 17:56:05 +0100

    - Avances.

--- pages/about/about.js
+++ pages/about/about.js
@@ -6,15 +6,3 @@ document.addEventListener(environment.dom.keys.onLoaded, function(event)
 {
     console.log("about.js");
 });
-
-// var body = document.body;
-// if (body.style.backgroundColor === "black") {
-//     body.style.backgroundColor = "white";
-//     switchButton.style.backgroundColor = "black";
-//     switchButton.style.color = "white";
-//     switchButton.innerHTML = "Cambiar a fondo negro";
-// } else {
-//     body.style.backgroundColor = "black";
-//     switchButton.style.backgroundColor = "white";
-//     switchButton.style.color = "black";
-//     switchButton.innerHTML = "Cambiar a fondo blanco";

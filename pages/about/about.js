d217eadb1562dc3d05aa59bdf8760413c4ace845
Kingdox ~ Xavier Arpa ~ <arpaxavier@gmail.com>
2023-03-12 23:39:45 +0100

    - Avances.

--- pages/about/about.js
+++ pages/about/about.js
@@ -2,7 +2,7 @@ import { environment } from "../../assets/js/environment.mjs";
 
 
 // Aquí va todo el código JS..
-document.addEventListener(environment.dom.keys.onLoaded, function(event) 
-{
-    console.log("about.js");
-});
+// document.addEventListener(environment.dom.keys.onLoaded, function(event) 
+// {
+//     console.log("about.js");
+// });
